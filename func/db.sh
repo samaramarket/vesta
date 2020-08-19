d5f7743372cb2e0a5a96ef3005b176d9744711bd
Alexey Semenov <a.semenov@timeweb.ru>
2020-08-19 17:42:15 +0300

    Revert "bump"
    This reverts commit 34a047615ff48faa651bcb126d0501f5e1e2bdd7.

--- func/db.sh
+++ func/db.sh
@@ -213,9 +213,13 @@ add_mysql_database() {
         IDENTIFIED BY '$dbpass'"
     mysql_query "$query" > /dev/null
 
-    query="SHOW GRANTS FOR \`$dbuser\`"
-    md5=$(mysql_query "$query" 2>/dev/null)
-    md5=$(echo "$md5" |grep 'PASSWORD' |tr ' ' '\n' |tail -n1 |cut -f 2 -d \')
+    if [ "$(echo $mysql_ver |cut -d '.' -f2)" -ge 7 ]; then
+        md5=$(mysql_query "SHOW CREATE USER \`$dbuser\`" 2>/dev/null)
+        md5=$(echo "$md5" |grep password |cut -f8 -d \')
+    else
+        md5=$(mysql_query "SHOW GRANTS FOR \`$dbuser\`" 2>/dev/null)
+        md5=$(echo "$md5" |grep PASSW|tr ' ' '\n' |tail -n1 |cut -f 2 -d \')
+    fi
 }
 
 # Create PostgreSQL database
